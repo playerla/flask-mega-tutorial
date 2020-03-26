fdb314a31a772bb2c5143ddb689528861a3af704
playerla <playerla.94@gmail.com>
2020-03-26 15:10:28 +0100

    Fix flask migrate error when loading app

--- core/main.py
+++ core/main.py
@@ -16,20 +16,23 @@ db.init_app(app)
 migrate = Migrate(app, db)
 # Sign up is disable so populate with some users
 with app.app_context():
-    if User.query.filter_by(username='admin').count() == 0:
-        u = User(
-            username='admin',
-            email='administrateur@microblog-flask-lopi.com',
-            password_hash="pbkdf2:sha256:150000$2XacMguc$bfc3752840f5e99164824768a597fcd582b59676ef154b29d70c5aaf874ce2ed"
-        )
-        u2 = User(
-            username='user',
-            email='user@microblog-flask-lopi.com',
-            password_hash="pbkdf2:sha256:150000$g6t07GxN$7b9b24a4a6d6b6f26a15ffa6e9e2ee333dbbfc7e0464fca0e45ccaf818bde3d4"
-        )
-        db.session.bulk_save_objects([u,u2])
-        db.session.commit()
-        print(f'{u} and {u2} created')
+    try:
+        if User.query.filter_by(username='admin').count() == 0:
+            u = User(
+                username='admin',
+                email='administrateur@microblog-flask-lopi.com',
+                password_hash="pbkdf2:sha256:150000$2XacMguc$bfc3752840f5e99164824768a597fcd582b59676ef154b29d70c5aaf874ce2ed"
+            )
+            u2 = User(
+                username='user',
+                email='user@microblog-flask-lopi.com',
+                password_hash="pbkdf2:sha256:150000$g6t07GxN$7b9b24a4a6d6b6f26a15ffa6e9e2ee333dbbfc7e0464fca0e45ccaf818bde3d4"
+            )
+            db.session.bulk_save_objects([u,u2])
+            db.session.commit()
+            print(f'{u} and {u2} created')
+    except:
+        print("Can't create default users")
 Bootstrap(app)
 init_auth(app)
 moment = Moment(app)
