052985bfdfbe91dfb9ed74ad3ce56a152041861d
playerla <playerla.94@gmail.com>
2020-03-26 11:55:31 +0100

    Populate with 'admin' and 'user' users for heroku

--- core/main.py
+++ core/main.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from config import config
 from flask_moment import Moment
 from flask_babel import Babel
-from core.models import db
+from core.models import db, User
 from core.auth.email import init_auth
 from core.search import CoreSearch
 
@@ -14,6 +14,22 @@ app.elasticsearch = CoreSearch(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
+# Sign up is disable so populate with some users
+with app.app_context():
+    if User.query.filter_by(username='admin').count() == 0:
+        u = User(
+            username='admin',
+            email='administrateur@microblog-flask-lopi.com',
+            password_hash="pbkdf2:sha256:150000$2XacMguc$bfc3752840f5e99164824768a597fcd582b59676ef154b29d70c5aaf874ce2ed"
+        )
+        u2 = User(
+            username='user',
+            email='user@microblog-flask-lopi.com',
+            password_hash="pbkdf2:sha256:150000$g6t07GxN$7b9b24a4a6d6b6f26a15ffa6e9e2ee333dbbfc7e0464fca0e45ccaf818bde3d4"
+        )
+        db.session.bulk_save_objects([u,u2])
+        db.session.commit()
+        print(f'{u} and {u2} created')
 Bootstrap(app)
 init_auth(app)
 moment = Moment(app)
