1998bc5e06e07a09ddec819d539a45931c4ee2ae
playerla <playerla.94@gmail.com>
2020-03-27 01:04:59 +0100

    Heroku: Postgresql

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 config = {
     'SECRET_KEY': os.environ.get('SECRET_KEY') or '86g746g63737g8d7438s6f',
-    'SQLALCHEMY_DATABASE_URI':'sqlite:///' + os.path.join(basedir, 'microblog.db'),
+    'SQLALCHEMY_DATABASE_URI': os.environ.get('DATABASE_URL') or ('sqlite:///' + os.path.join(basedir, 'microblog.db')),
     'SQLALCHEMY_TRACK_MODIFICATIONS':False,
     'MAIL_SERVER':'localhost',
     'ADMINS':['administrateur@microblog-flask-lopi.com'],
